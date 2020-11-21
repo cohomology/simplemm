a8e10f66dc08576488a509d4e9872379b7b52a59
Kilian Kilger <kkilger@gmail.com>
2020-11-21 21:04:20 +0100

    First steps in daemonization and logging

--- src/bin/simplemmd.rs
+++ src/bin/simplemmd.rs
@@ -1,11 +1,60 @@
-use snafu::ErrorCompat;
+#[macro_use]
+extern crate log;
+use snafu::{ErrorCompat, ResultExt};
+use simplemm::types::{Error, Config, Result, DaemonizeError, SyslogError, SetLoggerError};
+use syslog::{Facility, Formatter3164, BasicLogger};
+use log::LevelFilter;
+use daemonize::Daemonize;
+
+fn error_abort(error : Error) -> ! {
+    eprintln!("Error: {}", error); 
+    if let Some(backtrace) = ErrorCompat::backtrace(&error) {
+        eprintln!("{}", backtrace);
+    }
+    std::process::exit(-1);
+}
+
+fn daemonize(config : Config) -> Result<()> {
+     let daemonize = Daemonize::new()
+        .pid_file(&config.pid_file) 
+        .chown_pid_file(true)      
+        .working_directory(&config.working_dir)
+        .user(config.uid)
+        .group(config.gid)      
+        .umask(0o777);
+
+    daemonize.start().context(DaemonizeError {})?;
+    log_start(&config);
+    Ok(())
+}
+
+fn initialize_syslog() -> Result<()> {
+    let formatter = Formatter3164 {
+        facility: Facility::LOG_USER,
+        hostname: None,
+        process: "myprogram".into(),
+        pid: 0,
+    };
+
+    let logger = syslog::unix(formatter).context(SyslogError {})?;
+    log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
+        .map(|()| log::set_max_level(LevelFilter::Info)).context(SetLoggerError {})?;
+    Ok(())
+}
+
+fn log_start(config: &Config) {
+    info!("simplemmd started, uid = {}, gid = {}", config.uid, config.gid);
+}
+
+fn run() -> Result<()> {
+    initialize_syslog()?;  
+    let config = simplemm::config::read_config("simplemmd daemon")?;
+    simplemm::database::check_database(&config)?;
+    return daemonize(config);
+}
 
 fn main() {
-    let config = simplemm::config::read_config("simplemmd daemon");
-    if let Err(e) = config {
-      eprintln!("Error: {}", e); 
-      if let Some(backtrace) = ErrorCompat::backtrace(&e) {
-          println!("{}", backtrace);
-      }
+    if let Err(e) = run() {
+       error_abort(e)
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -7,6 +7,7 @@ pub struct Config {
     pub uid : u32,
     pub gid : u32,
     pub pid_file : String,
+    pub working_dir : String,
 }
 
 #[derive(Debug,Snafu)]
@@ -25,7 +26,19 @@ pub enum Error {
     #[snafu(display("Could not reach database: {}", source))]
     DbConnectionError {
         source: diesel::result::ConnectionError
-    } 
+    },
+    #[snafu(display("Could not daemonize: {}", source))]
+    DaemonizeError {
+        source: daemonize::DaemonizeError
+    },
+    #[snafu(display("Error connecting to syslog: {}", source))]
+    SyslogError {
+        source: syslog::Error
+    },
+    #[snafu(display("Error when initializing the logging subsystem: {}", source))]
+    SetLoggerError {
+        source: log::SetLoggerError
+    }
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
