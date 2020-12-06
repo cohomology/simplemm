74d50d1d0d944bba3c91ad2a9fe7a64a18046eed
Kilian Kilger <kkilger@gmail.com>
2020-12-06 22:47:54 +0100

    Trace sender

--- src/request.rs
+++ src/request.rs
@@ -30,6 +30,8 @@ fn handle_subscribe(command: types::Command) -> error::Result<()> {
         let mail = mailparse::parse_mail(data.as_bytes()).context(error::MailParseError {})?;
         let from = mail.headers.get_first_value("From");
         log::info!("Got mail from: {:?}", from);
+        let sender = mail.headers.get_first_value("Sender");
+        log::info!("Sender: {:?}", sender);
     } else {
         log::warn!("Subscribe without any data");
     }
